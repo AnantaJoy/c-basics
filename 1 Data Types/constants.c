a811712030b6cc0b0ab33cd96f5be97355ead4d0
Ananta Asim Joy <anantajoy007@gmail.com>
2022-06-24 01:02:27 +0600

    files added in Data Types

---
+++ 1 Data Types/constants.c
@@ -0,0 +1,4 @@
+// constants are defined with the #define directive
+// constants are used to define the value of a variable that is constant.
+// constants are values that are not changed.
+#include <stdio.h>
--- 1 Data Types/sizeof.c
+++ 1 Data Types/sizeof.c
@@ -12,6 +12,18 @@ int main()
     printf("sizeof float = %d\n", sizeof(float));
     printf("sizeof char = %d\n", sizeof(char));
     printf("sizeof double = %d\n", sizeof(double));
+    printf("sizeof long = %d\n", sizeof(long));
+    printf("sizeof short = %d\n", sizeof(short));
+    printf("sizeof long long = %d\n", sizeof(long long));
+    printf("sizeof unsigned int = %d\n", sizeof(unsigned int));
+    printf("sizeof unsigned char = %d\n", sizeof(unsigned char));
+    printf("sizeof unsigned long = %d\n", sizeof(unsigned long));
+    printf("sizeof unsigned short = %d\n", sizeof(unsigned short));
+    printf("sizeof unsigned long long = %d\n", sizeof(unsigned long long));
+    printf("sizeof signed char = %d\n", sizeof(signed char));
+    printf("sizeof signed long = %d\n", sizeof(signed long));
+    printf("sizeof signed short = %d\n", sizeof(signed short));
+    printf("sizeof long double = %d\n", sizeof(long double));
 
     return 0;
 }
